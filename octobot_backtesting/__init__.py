97392fdb0d93c3ff5ad79b0e3e2214ef7bfaa95c
Herklos <herklos.dev@protonmail.com>
2019-09-28 19:40:35 +0200

    Removed aiosqlite debug logs

--- octobot_backtesting/__init__.py
+++ octobot_backtesting/__init__.py
@@ -13,5 +13,8 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
+import logging
 
 from .constants import *
+
+logging.getLogger('aiosqlite').setLevel(logging.ERROR)
--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ ext_modules = [
 
 REQUIRED = open('requirements.txt').readlines()
 REQUIRES_PYTHON = '>=3.7'
+CYTHON_DEBUG = False if not os.getenv('CYTHON_DEBUG') else os.getenv('CYTHON_DEBUG')
 
 setup(
     name=PROJECT_NAME,
@@ -74,7 +75,7 @@ setup(
     data_files=[],
     setup_requires=REQUIRED,
     install_requires=[],
-    ext_modules=ext_modules,
+    ext_modules=cythonize(ext_modules, gdb_debug=CYTHON_DEBUG),
     python_requires=REQUIRES_PYTHON,
     classifiers=[
         'Development Status :: 3 - Alpha',
