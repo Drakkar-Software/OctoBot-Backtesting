05720d7fa1d883bb3494530d973abc6b3ea926b6
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-01-25 13:24:19 +0100

    update api

--- octobot_backtesting/api/importer.py
+++ octobot_backtesting/api/importer.py
@@ -13,15 +13,7 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
-from octobot_backtesting.collectors.exchanges.abstract_exchange_history_collector import \
-    AbstractExchangeHistoryCollector
-from octobot_commons.tentacles_management.advanced_manager import get_single_deepest_child_class
 
 
-async def collect_exchange_historical_data(exchange_name, symbols, time_frames=None) -> str:
-    collector_class = get_single_deepest_child_class(AbstractExchangeHistoryCollector)
-    collector_instance = collector_class({}, exchange_name, symbols, time_frames,
-                                         use_all_available_timeframes=time_frames is None)
-    await collector_instance.initialize()
-    await collector_instance.start()
-    return collector_instance.file_name
+def get_available_data_types(importer) -> list:
+    return importer.available_data_types
