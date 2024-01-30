8a73399e69aa33d37f5f98261419554506f4db82
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2024-01-31 00:09:22 +0100

    [Import] remove cleanup

--- octobot_backtesting/backtesting.py
+++ octobot_backtesting/backtesting.py
@@ -13,7 +13,6 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
-import uuid
 
 import async_channel.channels as channels
 import async_channel.util as channel_util
